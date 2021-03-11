3633fb76d4d287fbaf9fe9be24e0b85794529c76
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-03-11 12:36:19 +0100

    Add Eth and ER20 variant of the payment splitter + supper adding payee at any point

---
+++ contracts/finance/ERC20PaymentSplitter.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./PaymentSplitter.sol";
+import "../utils/Address.sol";
+import "../token/ERC20/utils/SafeERC20.sol";
+
+/**
+ * @title EthPaymentSplitter
+ * @dev An ERC20 specific version of the abstract {PaymentSplitter}
+
+ */
+contract ERC20PaymentSplitter is PaymentSplitter {
+    IERC20 immutable public token;
+
+    constructor (address token_, address[] memory payees_, uint256[] memory shares_) PaymentSplitter(payees_, shares_) {
+        require(Address.isContract(token_), "ERC20PaymentSplitter: token is not a contract");
+        token = IERC20(token_);
+    }
+
+    function _currentBalance() internal view virtual override returns (uint256) {
+        return token.balanceOf(address(this));
+    }
+
+    function _processPayment(address payable account, uint256 payment) internal virtual override {
+        SafeERC20.safeTransfer(token, account, payment);
+    }
+}
--- contracts/finance/PaymentSplitter.sol
+++ contracts/finance/PaymentSplitter.sol
@@ -2,24 +2,23 @@
 
 pragma solidity ^0.8.0;
 
-import "../utils/Address.sol";
 import "../utils/Context.sol";
-import "../utils/math/SafeMath.sol";
+import "../utils/Address.sol";
 
 /**
  * @title PaymentSplitter
- * @dev This contract allows to split Ether payments among a group of accounts. The sender does not need to be aware
- * that the Ether will be split in this way, since it is handled transparently by the contract.
+ * @dev This contract allows to split assets payments among a group of accounts. The sender does not need to be aware
+ * that the underlyint asset will be split in this way, since it is handled transparently by the contract.
  *
  * The split can be in equal parts or in any other arbitrary proportion. The way this is specified is by assigning each
- * account to a number of shares. Of all the Ether that this contract receives, each account will then be able to claim
+ * account to a number of shares. Of all the value that this contract receives, each account will then be able to claim
  * an amount proportional to the percentage of total shares they were assigned.
  *
  * `PaymentSplitter` follows a _pull payment_ model. This means that payments are not automatically forwarded to the
  * accounts but kept in this contract, and the actual transfer is triggered as a separate step by calling the {release}
  * function.
  */
-contract PaymentSplitter is Context {
+abstract contract PaymentSplitter is Context {
     event PayeeAdded(address account, uint256 shares);
     event PaymentReleased(address to, uint256 amount);
     event PaymentReceived(address from, uint256 amount);
@@ -38,27 +37,14 @@ contract PaymentSplitter is Context {
      * All addresses in `payees` must be non-zero. Both arrays must have the same non-zero length, and there must be no
      * duplicates in `payees`.
      */
-    constructor (address[] memory payees, uint256[] memory shares_) payable {
+    constructor (address[] memory payees_, uint256[] memory shares_) {
         // solhint-disable-next-line max-line-length
-        require(payees.length == shares_.length, "PaymentSplitter: payees and shares length mismatch");
-        require(payees.length > 0, "PaymentSplitter: no payees");
+        require(payees_.length == shares_.length, "PaymentSplitter: payees and shares length mismatch");
+        require(payees_.length > 0, "PaymentSplitter: no payees");
 
-        for (uint256 i = 0; i < payees.length; i++) {
-            _addPayee(payees[i], shares_[i]);
+        for (uint256 i = 0; i < payees_.length; ++i) {
+            _addPayee(payees_[i], shares_[i], 0);
         }
-    }
-
-    /**
-     * @dev The Ether received will be logged with {PaymentReceived} events. Note that these events are not fully
-     * reliable: it's possible for a contract to receive Ether without triggering this function. This only affects the
-     * reliability of the events, and not the actual splitting of Ether.
-     *
-     * To learn more about this see the Solidity documentation for
-     * https://solidity.readthedocs.io/en/latest/contracts.html#fallback-function[fallback
-     * functions].
-     */
-    receive () external payable virtual {
-        emit PaymentReceived(_msgSender(), msg.value);
     }
 
     /**
@@ -103,15 +89,15 @@ contract PaymentSplitter is Context {
     function release(address payable account) public virtual {
         require(_shares[account] > 0, "PaymentSplitter: account has no shares");
 
-        uint256 totalReceived = address(this).balance + _totalReleased;
+        uint256 totalReceived = _currentBalance() + _totalReleased;
         uint256 payment = totalReceived * _shares[account] / _totalShares - _released[account];
 
         require(payment != 0, "PaymentSplitter: account is not due payment");
 
-        _released[account] = _released[account] + payment;
-        _totalReleased = _totalReleased + payment;
+        _released[account] += payment;
+        _totalReleased += payment;
 
-        Address.sendValue(account, payment);
+        _processPayment(account, payment);
         emit PaymentReleased(account, payment);
     }
 
@@ -120,14 +106,40 @@ contract PaymentSplitter is Context {
      * @param account The address of the payee to add.
      * @param shares_ The number of shares owned by the payee.
      */
-    function _addPayee(address account, uint256 shares_) private {
+    function _addPayee(address account, uint256 shares_) internal virtual {
+        uint256 totalReceived = _currentBalance() + _totalReleased;
+        uint256 virtualrelease = _totalShares == 0 ? 0 : totalReceived * _shares[account] / _totalShares;
+
+        _addPayee(account, shares_, virtualrelease);
+    }
+
+    /**
+     * @dev Add a new payee to the contract.
+     * @param account The address of the payee to add.
+     * @param shares_ The number of shares owned by the payee.
+     * @param virtualrelease The offset used to balance splitting when adding a payee after construction
+     */
+    function _addPayee(address account, uint256 shares_, uint256 virtualrelease) private {
         require(account != address(0), "PaymentSplitter: account is the zero address");
         require(shares_ > 0, "PaymentSplitter: shares are 0");
         require(_shares[account] == 0, "PaymentSplitter: account already has shares");
 
         _payees.push(account);
         _shares[account] = shares_;
-        _totalShares = _totalShares + shares_;
+        _totalShares += shares_;
+        _released[account] = virtualrelease;
+        _totalReleased += virtualrelease;
+
         emit PayeeAdded(account, shares_);
     }
+
+    /**
+     * @dev abstract virtual function: returns the current balance of the PaymentSplitter
+     */
+    function _currentBalance() internal view virtual returns (uint256);
+
+    /**
+     * @dev abstract virtual function: send value/assets
+     */
+    function _processPayment(address payable account, uint256 payment) internal virtual;
 }
