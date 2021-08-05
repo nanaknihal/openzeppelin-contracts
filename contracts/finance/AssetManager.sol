6bed499a6798d375c8f149aa39d091ea7dff7a9a
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-08-05 10:06:40 +0200

    add erc721 and erc1155 support to AssetManager

--- contracts/finance/AssetManager.sol
+++ contracts/finance/AssetManager.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.0;
 import "../utils/Address.sol";
 import "../token/ERC20/IERC20.sol";
 import "../token/ERC20/utils/SafeERC20.sol";
+import "../token/ERC721/IERC721.sol";
+import "../token/ERC1155/IERC1155.sol";
 
 library AssetManager {
     struct Asset {
@@ -65,5 +67,50 @@ library AssetManager {
         uint256 value
     ) private {
         SafeERC20.safeTransfer(abi.decode(asset._data, (IERC20)), account, value);
+    }
+
+    /// ERC721
+    // solhint-disable-next-line func-name-mixedcase
+    function ERC721(IERC721 registry, uint256 tokenId) internal pure returns (Asset memory result) {
+        result._balanceHandler = _erc721Balance;
+        result._transferHandler = _erc721Transfer;
+        result._data = abi.encode(registry, tokenId);
+    }
+
+    function _erc721Balance(Asset memory asset, address account) private view returns (uint256) {
+        (IERC721 registry, uint256 tokenId) = abi.decode(asset._data, (IERC721, uint256));
+        return registry.ownerOf(tokenId) == account ? 1 : 0;
+    }
+
+    function _erc721Transfer(
+        Asset memory asset,
+        address account,
+        uint256 value
+    ) private {
+        require(value == 1, "Asset is non-fungible");
+        (IERC721 registry, uint256 tokenId) = abi.decode(asset._data, (IERC721, uint256));
+        registry.transferFrom(registry.ownerOf(tokenId), account, tokenId);
+    }
+
+    /// ERC1155
+    // solhint-disable-next-line func-name-mixedcase
+    function ERC1155(IERC1155 registry, uint256 tokenId) internal pure returns (Asset memory result) {
+        result._balanceHandler = _erc1155Balance;
+        result._transferHandler = _erc1155Transfer;
+        result._data = abi.encode(registry, tokenId);
+    }
+
+    function _erc1155Balance(Asset memory asset, address account) private view returns (uint256) {
+        (IERC1155 registry, uint256 tokenId) = abi.decode(asset._data, (IERC1155, uint256));
+        return registry.balanceOf(account, tokenId);
+    }
+
+    function _erc1155Transfer(
+        Asset memory asset,
+        address account,
+        uint256 value
+    ) private {
+        (IERC1155 registry, uint256 tokenId) = abi.decode(asset._data, (IERC1155, uint256));
+        registry.safeTransferFrom(address(this), account, tokenId, value, new bytes(0));
     }
 }
