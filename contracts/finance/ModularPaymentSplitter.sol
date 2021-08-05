9ce22a1d4a339d627accf6934ea2ff0566b35b2e
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-08-05 13:50:30 +0200

    document modular payment splitter

--- contracts/finance/ModularPaymentSplitter.sol
+++ contracts/finance/ModularPaymentSplitter.sol
@@ -1,33 +1,42 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.5;
 
 import "./AssetManager.sol";
 import "./SharesManager/ISharesManager.sol";
 
 /**
- * @title PaymentSplitter
- * @dev This contract allows to split Ether payments among a group of accounts. The sender does not need to be aware
- * that the Ether will be split in this way, since it is handled transparently by the contract.
+ * @title ModularPaymentSplitter
+ * @dev This contract allows to split payments in any fungible asset (supported by the AssetManager library) among a
+ * group of accounts. The sender does not need to be aware that the asset will be split in this way, since it is handled
+ * transparently by the contract.
  *
  * The split can be in equal parts or in any other arbitrary proportion. The way this is specified is by assigning each
- * account to a number of shares. Of all the Ether that this contract receives, each account will then be able to claim
- * an amount proportional to the percentage of total shares they were assigned.
+ * account to a number of shares through the {ISharesManager} interface. Of all the assets that this contract receives,
+ * each account will then be able to claim an amount proportional to the percentage of total shares they own assigned.
  *
- * `PaymentSplitter` follows a _pull payment_ model. This means that payments are not automatically forwarded to the
- * accounts but kept in this contract, and the actual transfer is triggered as a separate step by calling the {release}
- * function.
+ * `ModularPaymentSplitter` follows a _pull payment_ model. This means that payments are not automatically forwarded to
+ * the accounts but kept in this contract, and the actual transfer is triggered as a separate step by calling the
+ * {release} function.
  */
 abstract contract ModularPaymentSplitter is ISharesManager {
     using AssetManager for AssetManager.Asset;
 
+    // Asset handling functors, support ETH, ERC20 & ERC1155
     AssetManager.Asset private _asset;
 
+    // Release manifest
     mapping(address => uint256) private _released;
     uint256 private _totalReleased;
 
+    /**
+     * @dev Emitted when `amount` units are released to `to`.
+     */
     event PaymentReleased(address to, uint256 amount);
 
+    /**
+     * @dev Initialize with an asset handling object.
+     */
     constructor(AssetManager.Asset memory asset) {
         _asset = asset;
     }
@@ -47,8 +56,7 @@ abstract contract ModularPaymentSplitter is ISharesManager {
     }
 
     /**
-     * @dev Triggers a transfer to `account` of the amount of Ether they are owed, according to their percentage of the
-     * total shares and their previous withdrawals.
+     *
      */
     function pendingRelease(address account) public view virtual returns (uint256) {
         uint256 totalReceived = _asset.getBalance(address(this)) + totalReleased();
@@ -56,6 +64,10 @@ abstract contract ModularPaymentSplitter is ISharesManager {
         return personalValue - released(account);
     }
 
+    /**
+     * @dev Triggers a transfer of asset to `account` according to their percentage of the total shares and their
+     * previous withdrawals.
+     */
     function release(address account) public virtual {
         uint256 toRelease = pendingRelease(account);
         if (toRelease > 0) {
@@ -67,6 +79,11 @@ abstract contract ModularPaymentSplitter is ISharesManager {
         }
     }
 
+    /**
+     * @dev Hook to update the release manifest if shares distribution change after some assets have already been
+     * received. This hook should be called when updating the shares. For example, for ERC20 based shares, this hook
+     * should be triggered by the {ERC20-_beforeTokenTransfer} hook.
+     */
     function _sharesChanged(
         address account,
         uint256 oldShares,
